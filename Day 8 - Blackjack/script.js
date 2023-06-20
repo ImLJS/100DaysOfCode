3a2a68198344828eae9d041b6d12b99333813e2d
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2023-06-20 18:14:43 +0530

    Day 8 - Blackjack Game

--- Day 8 - Blackjack/script.js
+++ Day 8 - Blackjack/script.js
@@ -5,17 +5,40 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-function startGame(){
+
+function startGame() {
+    renderGame()
+}
+
+function renderGame(){
+
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+    sumEl.textContent = "Sum: " + sum;
 
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card? "
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = "You've got Blackjack! "
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game!"
         isAlive = false
     }
-    console.log(message)
+    
+    messageEl.textContent = message
+}
+
+function newCard() {
+
+    let card = 8
+    sum += card
+    cardsEl.textContent += " " + card
+    sumEl.textContent = "Sum: " + sum
+
+    renderGame()
+
 }
