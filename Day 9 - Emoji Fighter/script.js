b023539b16aae57052b83625907736f747ac08a7
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2023-06-23 07:44:51 +0530

    Day 9 - Emoji Fighter

--- Day 9 - Emoji Fighter/script.js
+++ Day 9 - Emoji Fighter/script.js
@@ -4,13 +4,9 @@ let stageEl = document.getElementById("stage")
 let fightButton = document.getElementById("fightButton")
 
 fightButton.addEventListener("click", function() {
-    // Challenge:
-    // When the user clicks on the "Pick Fighters" button, pick two random 
-    // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
     stageEl.textContent = fighters[randomFighter()] + 'vs' + fighters[randomFighter()]
 })
 
 function randomFighter(){
-    let fighter = Math.floor(Math.random()*17)
-    return fighter
+    return Math.floor(Math.random()*17)
 }
