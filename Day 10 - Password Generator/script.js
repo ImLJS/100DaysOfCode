f979d8fe556740409d1b3754fe0f14b8f658256b
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2023-06-23 10:44:02 +0530

    Day 10 - Password Generator

---
+++ Day 10 - Password Generator/script.js
@@ -0,0 +1,22 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U",
+                   "V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
+                   "q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8",
+                   "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",
+                   ",","|",":",";","<",">",".","?","/"];
+
+let generateButton = document.getElementById("generate");
+let passwordEl1 = document.getElementById("password-row-1");
+let passwordEl2 = document.getElementById("password-row-2");
+
+generateButton.addEventListener("click", function(){
+    passwordEl1.textContent = generatePassword();
+    passwordEl2.textContent = generatePassword();
+})
+
+function generatePassword(){
+    let password = "";
+    for(let i = 0; i < 10; i++){
+        password += characters[Math.floor(Math.random() * characters.length)];
+    }
+    return password;
+}
