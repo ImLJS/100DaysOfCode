4cf97059b5fd5702e23a47eea7d2afed52728cba
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2023-07-05 18:23:30 +0530

    Day 15

--- Day 15 - Tab Saver Chrome Extension/script.js
+++ Day 15 - Tab Saver Chrome Extension/script.js
@@ -1,4 +1,4 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
@@ -12,7 +12,7 @@ inputBtn.addEventListener("click", function() {
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        listItems += `<li><a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a></li>`
     }
     ulEl.innerHTML = listItems  
 }
