83513beff238109febc5acd56cf69cfecea32b5b
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2023-07-06 11:34:34 +0530

    Day 17 - AddToCart Mobile App

---
+++ Day 17 - AddToCart Mobile App/script.js
@@ -0,0 +1,6 @@
+const add_btn = document.getElementById("add-button");
+
+add_btn.addEventListener("click", function(){
+    let input = document.getElementById("input-field").value;
+    console.log(input);
+})
