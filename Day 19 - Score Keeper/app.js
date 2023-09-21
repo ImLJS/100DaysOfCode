fdc745c130419ac2e180e508db5d644815b6c007
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2023-09-21 08:05:42 +0530

    Day 19 - Score Keeper

--- Day 19 - Score Keeper/app.js
+++ Day 19 - Score Keeper/app.js
@@ -1,50 +1,51 @@
 const playto = document.querySelector("#playto");
-const p1Display = document.querySelector("#p1Display");
-const p2Display = document.querySelector("#p2Display");
-const p1Button = document.querySelector("#p1Button");
-const p2Button = document.querySelector("#p2Button");
 const reset = document.querySelector("#reset");
+
+const p1 = {
+  score: 0,
+  display: document.querySelector("#p1Display"),
+  button: document.querySelector("#p1Button"),
+};
+
+const p2 = {
+  score: 0,
+  display: document.querySelector("#p2Display"),
+  button: document.querySelector("#p2Button"),
+};
 
 let winCondition = 3;
 
 function resetGame() {
-  p1Button.disabled = false;
-  p2Button.disabled = false;
-  p1Display.style.color = "black";
-  p2Display.style.color = "black";
-  p1Display.textContent = 0
-  p2Display.textContent = 0
-};
+  p1.button.disabled = false;
+  p2.button.disabled = false;
+  p1.display.style.color = "black";
+  p2.display.style.color = "black";
+  p1.display.textContent = 0;
+  p2.display.textContent = 0;
+}
 
-playto.addEventListener("input", function (e) {
+function playGame(player, opponent) {
+  player.score = parseInt(player.display.textContent) + 1;
+  player.display.textContent = player.score;
+  if (player.score === parseInt(winCondition)) {
+    player.button.disabled = true;
+    opponent.button.disabled = true;
+    player.display.style.color = "green";
+    opponent.display.style.color = "red";
+  }
+}
+
+playto.addEventListener("change", function (e) {
   winCondition = this.value;
-  console.log(winCondition);
-  console.log(typeof winCondition);
-  p1Display.textContent = 0
-  p2Display.textContent = 0
+  resetGame();
 });
 
-p1Button.addEventListener("click", function () {
-  const p1score = parseInt(p1Display.textContent) + 1;
-  p1Display.textContent = p1score;
-  console.log(winCondition);
-  if (p1score === parseInt(winCondition)) {
-    p1Button.disabled = true;
-    p2Button.disabled = true;
-    p1Display.style.color = "green";
-    p2Display.style.color = "red";
-  }
+p1.button.addEventListener("click", function () {
+  playGame(p1, p2);
 });
 
-p2Button.addEventListener("click", function () {
-  const p2score = parseInt(p2Display.textContent) + 1;
-  p2Display.textContent = p2score;
-  if (p2score === parseInt(winCondition)) {
-    p1Button.disabled = true;
-    p2Button.disabled = true;
-    p2Display.style.color = "green";
-    p1Display.style.color = "red";
-  }
+p2.button.addEventListener("click", function () {
+  playGame(p2, p1);
 });
 
 reset.addEventListener("click", resetGame);
